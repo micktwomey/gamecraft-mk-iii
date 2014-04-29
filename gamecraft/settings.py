07104cff94349426fad8d64a3437ca7890192e71
Michael Twomey <micktwomey@gmail.com>
2014-04-29 23:32:23 +0100

    Disable react in django pipeline
    Not using at the moment, disabling as triggers an encoding bug.

--- gamecraft/settings.py
+++ gamecraft/settings.py
@@ -146,7 +146,7 @@ PIPELINE_JS = {
             'js/jquery.js',
             'js/bootstrap.js',
             'js/leaflet.js',
-            'js/react-with-addons.js',
+            # 'js/react-with-addons.js',  # bug in processing the '\uFEFF' in the file
         ),
         'output_filename': 'js/gamecraft.js',
     },
