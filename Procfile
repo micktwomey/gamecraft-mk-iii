0ba928dde47266db6308ef6b7555fb68925adfaa
Michael Twomey <mick@twomeylee.name>
2014-06-26 23:25:34 +0100

    Heroku-ifying gamecraft.it
    If heroku works I'm gonna switch to that, it's even cheaper (AKA free).

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gamecraft.wsgi_heroku
---
+++ gamecraft/wsgi_heroku.py
@@ -0,0 +1,7 @@
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "gamecraft.settings_heroku")
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+application = Cling(get_wsgi_application())
