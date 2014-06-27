aaddea2c9fa3519a5f8a85e5a6b1314f781efd99
Michael Twomey <michael.twomey@fieldaware.com>
2014-06-27 13:40:07 +0100

    Experimenting with different static file settings

--- gamecraft/settings_heroku.py
+++ gamecraft/settings_heroku.py
@@ -16,11 +16,12 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+# STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 )
 
 INSTALLED_APPS = INSTALLED_APPS + (
