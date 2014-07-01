18e5f0b2a37dd2b45abd0ac4dbd1fbf9cfcb4715
Michael Twomey <mick@twomeylee.name>
2014-07-01 22:49:29 +0100

    Adding redis caching and session storage

--- gamecraft/settings_heroku.py
+++ gamecraft/settings_heroku.py
@@ -1,11 +1,15 @@
+import os
+import urllib.parse
+
+import dj_database_url
+
 from gamecraft.settings import *
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 
 # Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
@@ -14,7 +18,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = ['*']
 
 # Static asset configuration
-import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
@@ -26,3 +29,22 @@ STATICFILES_DIRS = (
 INSTALLED_APPS = INSTALLED_APPS + (
     'raven.contrib.django.raven_compat',
 )
+
+redis_parse_result = urllib.parse.urlparse(os.environ['REDISCLOUD_URL'])
+CACHES = {
+    'default': {
+        'BACKEND': 'redis_cache.cache.RedisCache',
+        'LOCATION': '{redis.hostname}:{redis.port}:0'.format(redis=redis_parse_result),
+        'OPTIONS': {
+            'CLIENT_CLASS': 'redis_cache.client.DefaultClient',
+            'PASSWORD': redis_parse_result.password,
+            'PICKLE_VERSION': -1,
+            'IGNORE_EXCEPTIONS': True,
+            'CONNECTION_POOL_KWARGS': {'max_connections': 10}
+        }
+    }
+}
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
+
+SESSION_CACHE_ALIAS = 'default'
