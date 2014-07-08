c818847f96074ecd15128b1a649d17497170e986
Michael Twomey <mick@twomeylee.name>
2014-07-09 00:12:01 +0100

    Adding ical feeds for events
    Look under /events/ical/ for the feed.

---
+++ gamecraft/gamecrafts/ical_feeds.py
@@ -0,0 +1,35 @@
+"""iCal Calendar feeds
+
+"""
+
+from django_ical.views import ICalFeed
+from gamecraft.gamecrafts.models import GameCraft
+
+
+class GameCraftICalFeed(ICalFeed):
+    """GameCraft Event Calendar
+    """
+
+    product_id = '-//gamecraft.it//GameCraft//EN'
+    timezone = 'UTC'
+
+    def items(self):
+        return GameCraft.published.all().order_by('-starts')
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.content
+
+    def item_start_datetime(self, item):
+        return item.starts
+
+    def item_end_datetime(self, item):
+        return item.ends
+
+    def item_location(self, item):
+        return item.location
+
+    def item_geolcoation(self, item):
+        return (item.location_latitude, item.location_longitude)
--- gamecraft/gamecrafts/urls.py
+++ gamecraft/gamecrafts/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import patterns, include, url
 
 from gamecraft.gamecrafts import feeds
+from gamecraft.gamecrafts import ical_feeds
 
 urlpatterns = patterns('gamecraft.gamecrafts',
     url(r'^$', 'views.list_gamecrafts', name='list_gamecrafts'),
     url(r'^rss/$', feeds.GameCraftRSSFeed(), name="gamecraft_rss"),
     url(r'^new/$', 'views.new_gamecraft', name='new_gamecraft'),
+    url(r'^ical/$', ical_feeds.GameCraftICalFeed(), name="gamecraft_ical"),
     url(r'^(?P<slug>[^/]+)/edit/$', 'views.edit_gamecraft', name='edit_gamecraft'),
     url(r'^(?P<slug>[^/]+)/$', 'views.view_gamecraft', name='view_gamecraft'),
     url(r'^(?P<slug>[^/]+)/background/$', 'views.view_background', name='view_gamecraft_background'),
